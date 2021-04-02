30ea098ca769f1a0bd185c54a5aff083e72cc264
Alex Sandri <me@alexsandri.com>
2021-04-02 09:05:27 +0200

    Add user related tables

--- schema.sql
+++ schema.sql
@@ -1,10 +1,47 @@
-------------
--- TABLES --
-------------
+-------------
+-- TABELLE --
+-------------
 
-create table "users"
+create table "utenti"
 (
     "id" text not null,
-    "email" text not null,
-    ""
+    "nome" text not null,
+    "cognome" text not null,
+    "email" varchar(320) not null,
+    "data_nascita" date not null,
+    "codice_fiscale" char(16) not null,
+
+    primary key ("id"),
+
+    unique ("email"),
+    unique ("codice_fiscale"),
+
+    check ("data_nascita" < current_date)
+);
+
+create table "portafogli"
+(
+    "id" text not null,
+    "utente" text not null,
+    "credito" numeric(10, 2) not null,
+
+    primary key ("id"),
+
+    unique ("utente"),
+
+    foreign key ("utente") references "utenti" on update cascade on delete cascade
+
+    check ("credito" >= 0)
+);
+
+create table "metodi_pagamento"
+(
+    "id" text not null,
+    "tipo" text not null,
+    "dati" json not null,
+    "portafoglio" text not null,
+
+    primary key ("id"),
+
+    foreign key ("portafoglio") references "portafogli" on update cascade on delete cascade
 );
