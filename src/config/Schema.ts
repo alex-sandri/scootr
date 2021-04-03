fdd1cd87fc74b6b0d3ebfee85d1e28176906fe6b
Alex Sandri <me@alexsandri.com>
2021-04-03 12:15:13 +0200

    Create Schema class

---
+++ src/config/Schema.ts
@@ -0,0 +1,11 @@
+import Joi from "joi";
+
+export class Schema
+{
+    public static readonly EMAIL = Joi; // TODO
+
+    public static readonly DATE = Joi; // TODO
+    public static readonly DATETIME = Joi; // TODO
+
+    public static readonly CODICE_FISCALE = Joi; // TODO
+}
