61e42fac9e24001f5186a500af60c3f6faee8d0a
Alex Sandri <me@alexsandri.com>
2021-04-03 14:40:29 +0200

    Add POST /webhooks/stripe

--- src/routes/webhooks/index.ts
+++ src/routes/webhooks/index.ts
@@ -1,6 +1,61 @@
 import Boom from "@hapi/boom";
 import { ServerRoute } from "@hapi/hapi";
+import Stripe from "stripe";
+import { Config } from "../../config/Config";
+import Database from "../../utilities/Database";
 
 export default <ServerRoute[]>[
-    
+    {
+        method: "POST",
+        path: "/webhooks/stripe",
+        options: {
+            auth: false,
+            payload: {
+                output: "data",
+                parse: false,
+            },
+        },
+        handler: async (request, h) =>
+        {
+            let event: Stripe.Event;
+
+            try
+            {
+                event = Config.STRIPE.webhooks.constructEvent(
+                    request.payload as any,
+                    request.headers["stripe-signature"],
+                    process.env.STRIPE_WEBHOOK_SECRET ?? "",
+                );
+            }
+            catch (err)
+            {
+                throw Boom.forbidden();
+            }
+
+            switch (event.type)
+            {
+                case "customer.created":
+                {
+                    const customer = event.data.object as Stripe.Customer;
+
+                    await Database.pool
+                        .query(
+                            `update "utenti" set "stripe_customer_id" = $1 where "id" = $2`,
+                            [
+                                customer.id,
+                                customer.metadata.user_id,
+                            ],
+                        )
+                        .catch(() =>
+                        {
+                            throw Boom.badImplementation();
+                        });
+
+                    break;
+                }
+            }
+
+            return { received: true };
+        },
+    },
 ];
