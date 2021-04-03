9933deda56d14d647625db1b1b5755ac1db784d6
Alex Sandri <me@alexsandri.com>
2021-04-03 18:31:12 +0200

    Add missing prefixes

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -6,6 +6,11 @@ export class Config
 
     public static readonly ID_PREFIXES = {
         USER: "usr",
+        WALLET: "wlt",
+        PAYMENT_METHOD: "pmt",
+        VEHICLE: "vcl",
+        RIDE: "rid",
+        SESSION: "ses",
     } as const;
 
     public static readonly STRIPE = new Stripe(process.env.STRIPE_SECRET_API_KEY ?? "", { apiVersion: "2020-08-27" });
