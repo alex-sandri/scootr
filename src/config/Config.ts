0d2b10eec97e42a0721cf4c3a1c523cc29d68eb1
Alex Sandri <me@alexsandri.com>
2021-04-03 18:41:51 +0200

    Add some Config props that will be used in auth

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -2,6 +2,24 @@ import Stripe from "stripe";
 
 export class Config
 {
+    public static readonly IS_PRODUCTION = process.env.NODE_ENV === "production";
+
+    /**
+     * @default
+     * 
+     * 30 days
+     */
+    public static readonly SESSION_DURATION = 60 * 60 * 24 * 30;
+
+    /**
+     * @default
+     * 
+     * 5 minutes
+     */
+    public static readonly SIGN_IN_REQUEST_DURATION = 60 * 5;
+
+    public static readonly SIGN_IN_REQUEST_TOKEN_BYTES = 60;
+
     public static readonly USER_MIN_AGE = 18;
 
     public static readonly ID_PREFIXES = {
