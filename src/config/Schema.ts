657333ce8335f8770e58be11213081d2d0a8d036
Alex Sandri <me@alexsandri.com>
2021-04-03 12:25:34 +0200

    Add some schemas and add GET /utenti/id

--- src/config/Schema.ts
+++ src/config/Schema.ts
@@ -2,7 +2,11 @@ import Joi from "joi";
 
 export class Schema
 {
-    public static readonly EMAIL = Joi; // TODO
+    public static readonly STRING = Joi.string().trim();
+
+    public static readonly ID = (prefix: string) => Schema.STRING.pattern(new RegExp(`^${prefix}_.+$`));
+
+    public static readonly EMAIL = Schema.STRING.email();
 
     public static readonly DATE = Joi; // TODO
     public static readonly DATETIME = Joi; // TODO
--- src/routes/utenti/index.ts
+++ src/routes/utenti/index.ts
@@ -1,6 +1,21 @@
 import Boom from "@hapi/boom";
 import { ServerRoute } from "@hapi/hapi";
+import Joi from "joi";
+import { Config } from "../../config/Config";
+import { Schema } from "../../config/Schema";
 
 export default <ServerRoute[]>[
-    
+    {
+        method: "GET",
+        path: "/utenti/{id}",
+        options: {
+            validate: {
+                params: Joi.object({
+                    id: Schema.ID(Config.PREFISSI_ID.UTENTE).required(),
+                }),
+            },
+        },
+        handler: async (request, h) =>
+        {},
+    },
 ];
