58ea415acf1c9a5e0701ae352b76be4113886101
Alex Sandri <me@alexsandri.com>
2021-04-04 11:45:08 +0200

    Add vehicles routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import auth from "./auth";
 import sessions from "./sessions";
 import users from "./users";
+import vehicles from "./vehicles";
 import webhooks from "./webhooks";
 
 export = [
     auth,
     sessions,
     users,
+    vehicles,
     webhooks,
 ].flat();
