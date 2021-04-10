e61367447b02843b19edc812a6a1014b2b19bc7b
Alex Sandri <me@alexsandri.com>
2021-04-10 19:12:40 +0200

    Register wallets routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import sessions from "./sessions";
 import users from "./users";
 import vehicles from "./vehicles";
+import wallets from "./wallets";
 import webhooks from "./webhooks";
 
 export = [
     sessions,
     users,
     vehicles,
+    wallets,
     webhooks,
 ].flat();
