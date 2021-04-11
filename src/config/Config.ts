6af5f3cbd9bb0b327b18aebe798a04f6437b8bc5
Alex Sandri <me@alexsandri.com>
2021-04-11 11:43:54 +0200

    Update

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -4,13 +4,6 @@ export class Config
 {
     public static readonly IS_PRODUCTION = process.env.NODE_ENV === "production";
 
-    /**
-     * @default
-     * 
-     * 30 days
-     */
-    public static readonly SESSION_DURATION = 60 * 60 * 24 * 30;
-
     public static readonly ID_PREFIXES = {
         USER: "usr",
         WALLET: "wlt",
@@ -22,6 +15,13 @@ export class Config
     } as const;
 
     public static readonly ID_BYTE_LENGTH = 30;
+
+    /**
+     * @default
+     * 
+     * 30 days
+     */
+    public static readonly SESSION_DURATION = 60 * 60 * 24 * 30;
 
     public static readonly STRIPE = new Stripe(process.env.STRIPE_SECRET_API_KEY ?? "", { apiVersion: "2020-08-27" });
 
