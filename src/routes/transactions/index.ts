60df91bc6fc4ab33ff9d9cc50697e6a29e52a0d5
Alex Sandri <me@alexsandri.com>
2021-04-16 14:29:04 +0200

    Add transaction endpoints

---
+++ src/routes/transactions/index.ts
@@ -0,0 +1,43 @@
+import Boom from "@hapi/boom";
+import { ServerRoute } from "@hapi/hapi";
+import Joi from "joi";
+import { Schema } from "../../config/Schema";
+
+export default <ServerRoute[]>[
+    {
+        method: "GET",
+        path: "/transactions/{id}",
+        options: {
+            validate: {
+                params: Joi.object({
+                    id: Schema.ID.TRANSACTION.required(),
+                }),
+            },
+            response: {
+                schema: Transaction.SCHEMA.OBJ,
+            },
+        },
+        handler: async (request, h) =>
+        {
+            throw Boom.notImplemented();
+        },
+    },
+    {
+        method: "GET",
+        path: "/wallets/{id}/transactions",
+        options: {
+            validate: {
+                params: Joi.object({
+                    id: Schema.ID.WALLET.required(),
+                }),
+            },
+            response: {
+                schema: Schema.ARRAY(Transaction.SCHEMA.OBJ),
+            },
+        },
+        handler: async (request, h) =>
+        {
+            throw Boom.notImplemented();
+        },
+    },
+];
